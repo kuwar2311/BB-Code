e41ea4c0e13bbc60747b5d217a44bd2c0b8cec6d
kuwar2311 <38282390+kuwar2311@users.noreply.github.com>
2023-01-20 15:30:31 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,11 @@ mv -f BB-Code/Code/* /home/cdac/BB_Code_1_5/Code/
 mv -f BB-Code/ReadMe.txt /home/cdac/BB_Code_1_5/
 cd /home/cdac/BB_Code_1_5/
 make
+rm -rf logs/*
 
 sudo su
 mv -f BB-Code/Code/* /home/cdac/Desktop/Client-Code/BB_Code_1_5/Code/
 mv -f BB-Code/ReadMe.txt /home/cdac/Desktop/Client-Code/BB_Code_1_5/
 cd /home/cdac/Desktop/Client-Code/BB_Code_1_5/
 make
+rm -rf logs/*
