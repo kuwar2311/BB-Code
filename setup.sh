994120e7c2b176945c7f3e839bb3fea7ecb4c873
kuwar2311 <38282390+kuwar2311@users.noreply.github.com>
2023-01-20 17:26:39 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 DIR="/home/cdac/BB_Code_1_5/Code/"
 if [ -d "$DIR" ]; then
   echo "'/home/cdac/BB_Code_1_5/' folder exist ..."
+  chown -R cdac:cdac /home/cdac/BB_Code_1_5/
   mv -f BB-Code/Code/* /home/cdac/BB_Code_1_5/Code/
   mv -f BB-Code/ReadMe.txt /home/cdac/BB_Code_1_5/
   cd /home/cdac/BB_Code_1_5/
@@ -10,6 +11,7 @@ else
   DIR="/home/cdac/Desktop/Client-Code/BB_Code_1_5/Code/"
   if [ -d "$DIR" ]; then
     echo "'/home/cdac/Desktop/Client-Code/BB_Code_1_5/Code/' folder doesn't exist ..."
+    chown -R cdac:cdac /home/cdac/Desktop/Client-Code/
     mv -f BB-Code/Code/* /home/cdac/Desktop/Client-Code/BB_Code_1_5/Code/
     mv -f BB-Code/ReadMe.txt /home/cdac/Desktop/Client-Code/BB_Code_1_5/
     cd /home/cdac/Desktop/Client-Code/BB_Code_1_5/
