91b48c8acdb37e0c4db3d484ca243ca75279449a
kuwar2311 <38282390+kuwar2311@users.noreply.github.com>
2023-01-20 14:43:45 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#sudo git clone  https://github.com/kuwar2311/BB-Code.git
+sudo su
+mv -f BB-Code/Code/* /home/cdac/BB_Code_1_5/Code/
+mv -f BB-Code/ReadMe.txt /home/cdac/BB_Code_1_5/
+cd /home/cdac/BB_Code_1_5/
+make
