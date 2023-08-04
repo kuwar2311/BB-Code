4fcbd5f1d8c5201a3a562cf32db77ed5a03fd7dd
kuwar2311 <38282390+kuwar2311@users.noreply.github.com>
2023-08-04 20:36:04 +0530

    Update ns_setup.sh

--- ns_setup.sh
+++ ns_setup.sh
@@ -2,8 +2,8 @@ DIR="/home/pi/BB_Code_1_5/Code/"
 if [ -d "$DIR" ]; then
   echo "'/home/pi/BB_Code_1_5/' folder exist ..."
   chown -R pi:pi /home/pi/BB_Code_1_5/
-  mv -f /BB-Code/Code/* /home/pi/BB_Code_1_5/Code/
-  mv -f /BB-Code/ReadMe.txt /home/pi/BB_Code_1_5/
+  mv -f BB-Code/Code/* /home/pi/BB_Code_1_5/Code/
+  mv -f BB-Code/ReadMe.txt /home/pi/BB_Code_1_5/
   cd /home/pi/BB_Code_1_5/
   make clean
   make
@@ -13,8 +13,8 @@ else
   if [ -d "$DIR" ]; then
     echo "'/home/pi/Desktop/Client-Code/BB_Code_1_5/Code/' folder doesn't exist ..."
     chown -R pi:pi /home/pi/Desktop/Client-Code/
-    mv -f /BB-Code/Code/* /home/pi/Desktop/Client-Code/BB_Code_1_5/Code/
-    mv -f /BB-Code/ReadMe.txt /home/pi/Desktop/Client-Code/BB_Code_1_5/
+    mv -f BB-Code/Code/* /home/pi/Desktop/Client-Code/BB_Code_1_5/Code/
+    mv -f BB-Code/ReadMe.txt /home/pi/Desktop/Client-Code/BB_Code_1_5/
     cd /home/pi/Desktop/Client-Code/BB_Code_1_5/
     make clean
     make
